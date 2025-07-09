77a3df62f7fc4a90a2443b8815c794819fcdbcf3
Fazliddin <unknown0436177@gmail.com>
2025-07-09 19:40:50 +0500

    update generator method

--- src/main/java/org/example/uzum_market/service/MailingService.java
+++ src/main/java/org/example/uzum_market/service/MailingService.java
@@ -1,7 +1,7 @@
 package org.example.uzum_market.service;
 
 public class MailingService {
-    public String generateVerificationCode(String email) {
+    public String generateVerificationCode() {
 
     }
 }
